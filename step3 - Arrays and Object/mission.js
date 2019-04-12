969f8a5230798513824a15407d0b407f748fbe35
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-12 16:27:29 +0900

    step 3 : mission 2 구현 -> 다중 배열 객체 찾기
    배열결과 출력 - type이 sk인, name으로 구성된 배열만 출력

--- step3 - Arrays and Object/mission.js
+++ step3 - Arrays and Object/mission.js
@@ -11,11 +11,56 @@ const isNumber = (val) => {
 }
 
 const answer = [];
-const missionArray = Object.values(missionData1).forEach(obj => {
+const functionMission1 = Object.values(missionData1).forEach(obj => {
     Object.keys(obj).forEach(key => {
         if (isNumber(obj[key])) 
         answer.push(key);
     })
 });
 
-console.log(answer);
+// 2. 배열결과 출력 - type이 sk인, name으로 구성된 배열만 출력해본다.
+
+
+// const flatten = (arr) => {
+//     const stack = [...arr];
+//     const res = [];
+
+//     while (stack.length) {
+//         const next = stack.pop();
+//         if (Array.isArray(next)) {
+//             stack.push(...next);
+//         } else {
+//             res.push(next);
+//         }
+//     }
+
+//     return res.reverse();
+// }
+
+const namesArr = [];
+
+// 정답 배열에 insert
+const insertArray = (name) => {
+    namesArr.push(name);
+}
+
+// 타입 체크
+const isTypeCheck = (type) => {
+    return type === "sk";
+}
+
+// 타입 체크 후 배열 insert
+const checkAndInsert = (type, name) => {
+    if (isTypeCheck(type)) {
+        insertArray(name);
+    } 
+}
+
+// 재귀
+const findObj = (arr) => arr.forEach(obj => {
+    checkAndInsert(obj.type, obj.name, namesArr)
+    if (obj.childnode) findObj(obj.childnode);
+});
+
+const functionMission2 = findObj(missionData2);
+console.log(namesArr);
