fa84b790e33f9f4be73e11ad428e16a14c697bcd
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-19 16:40:38 +0900

    정규표현식으로 명령어 추출 및 실행 기능 추가
    정규표현식으로 명령어를 배열로 리턴하는 함수 추가
    명령어 배열을 받아 명령어를 실행하는 함수 추가

--- step4 - design and async programming/step4-2 - async programming/mission/grade.js
+++ step4 - design and async programming/step4-2 - async programming/mission/grade.js
@@ -1,7 +1,38 @@
+//app.js
 const Todos = require('./todos');
 
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin,process.stdout);
+
+const regexp = () => {
+    // [1번째 command, 2번째 command]
+    const regexpForPrimaryCommand = /[^\$]+(?=\$)/g;
+    // [2번째 command, 3번째 command]
+    const regexpForSecondaryCommand = /(?<=\$)[^\$]+/g;
+    const regexpObj = {regexpForPrimaryCommand, regexpForSecondaryCommand};
+    
+    return regexpObj;
+};
+
+const getSeperateCommandArray = (command) => {
+    let commandArray = [];
+    
+    const firstCommand = command.match(regexp().regexpForPrimaryCommand)[0];
+    const secondCommand = command.match(regexp().regexpForSecondaryCommand)[0];
+    const thirdCommand = command.match(regexp().regexpForSecondaryCommand)[1];
+
+    commandArray = [firstCommand, secondCommand];
+
+    if (thirdCommand) {
+        commandArray.push(thirdCommand);
+    }
+    return commandArray;
+};
+
+
+const executeTodoMethod = (firstCommand, secondCommand, thirdCommand) => {
+    Todos[firstCommand](secondCommand, thirdCommand);
+};
 
 rl.setPrompt('명령하세요: ');
 rl.prompt();
@@ -12,6 +43,14 @@ rl.on('line', (userInput) => {
             break;
         default:
             console.log(`당신의 명령어: ${userInput}`);
+			const commandArr = getSeperateCommandArray(userInput);
+			
+            if (commandArr.length == 2) {
+               executeTodoMethod(commandArr[0], commandArr[1]);
+			} else {
+            	executeTodoMethod(commandArr[0], commandArr[1], commandArr[2]);
+            }
+
             break;
     }
     rl.prompt();
@@ -19,8 +58,6 @@ rl.on('line', (userInput) => {
     console.log("프로그램을 종료합니다.");
     process.exit();
 });
-
-
 
 
 
