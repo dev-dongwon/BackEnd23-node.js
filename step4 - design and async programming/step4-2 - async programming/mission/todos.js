8c0b0fea0a275c42d812866f5d5956cea584876a
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-22 09:02:30 +0900

    create todos.js

--- step4 - design and async programming/step4-2 - async programming/mission/todos.js
+++ step4 - design and async programming/step4-2 - async programming/mission/todos.js
@@ -1,100 +1,87 @@
 //todos.js
-const originData = require('./todoList.json');
+const originData = require('./todosList.json');
 const convertedData = JSON.parse(JSON.stringify(originData)).data;
-
-const status = ['todo', 'doing', 'done'];
 const idGenerator = () => Math.floor(Math.random() * (99999)) + 1;
 
-const searchById = (id) => {
-	let resultIndex = null;
-	convertedData.forEach((value, index) => {
-		if (value.id == id) resultIndex = index;
-	});
-	return resultIndex;
+const getArrByCondition = (arr, condition) => {
+    return arr.reduce((acc, val) => {
+        if (condition(val)) acc.push(val);
+        return acc;
+    }, []);
 };
 
-const addData = (name, tags)=> { 
+const showStatusLazy = () => {
+    setTimeout(() => {
+        showData('all');
+    }, 1000);
+}
+
+const isDuplicated = (val, key, convertedData) => {
+    let result = false;
+    convertedData.forEach((element) => {
+        if (val == element[key]) result = true;
+    })
+    return result;
+}
+
+const showData = (type) => {
+    if (type == 'all') {
+        const numOfTodos = getArrByCondition(convertedData, (val) => {return val.status == "todo"}).length;
+        const numOfDoings = getArrByCondition(convertedData, (val) => {return val.status == "doing"}).length;
+        const numOfDones = getArrByCondition(convertedData, (val) => {return val.status == "done"}).length;
+
+        console.log(`현재상태 : todo: ${numOfTodos}개, doing: ${numOfDoings}개, done: ${numOfDones}개`);
+
+    } else {
+        const objArr = getArrByCondition(convertedData, (val) => {return (type == val.status)});
+        let result = `${type} 리스트 : 총${objArr.length}건 :`;
+        getArrByCondition(objArr, (val) => {result += `, '${val.name}, ${val.id}번'`; return true});
+        console.log(`${result}`);
+    }
+}
+
+const addData = (name, tags) => {
+    const id = idGenerator();
+
+    if (isDuplicated(id, "id", convertedData)) {
+        return addData(name, tags);
+    }
+
     let obj = {
-        name,
-        tags,
-        status: 'todo',
-        id: idGenerator()
+        name, 
+        tags, 
+        status: 'todo', 
+        id,
     };
+
     convertedData.push(obj);
-	console.log(`${obj.name} 1개가 추가됐습니다.(id : ${obj.id})`);
-	setTimeout(() => {
-		show("all");
-	}, 1000);
-};
+    console.log(`${obj.name} 1개가 추가됐습니다.(id : ${obj.id})`);
+    showStatusLazy();
+}
 
 const deleteData = (id)=> { 
-	const targetIndex = searchById(id);
-	const targetStatus = convertedData[targetIndex].status;
-	const targetName = convertedData[targetIndex].name;
-	
-	convertedData.splice(targetIndex, 1);
-	console.log(`${targetName} ${targetStatus}가 목록에서 삭제됐습니다.`);
-	setTimeout(() => {
-		show("all");
-	}, 1000);
+    const target = getArrByCondition(convertedData, (val) => {return val.id == id})[0];
+    if (!target) {console.log('일치하는 id가 없습니다'); return;}
+
+    convertedData.splice(convertedData.indexOf(target), 1);
+    console.log(`${target.name} ${target.status}가 목록에서 삭제됐습니다.`);
+    showStatusLazy();
 };
 
+const updateData = (id, status)=> {
+    const target = getArrByCondition(convertedData, (val) => {return val.id == id})[0];
+    if (!target) {console.log('일치하는 id가 없습니다'); return;}
 
-const updateData = (id, status)=> { 
-	const targetIndex = searchById(id);
-	const targetName = convertedData[targetIndex].name;
-	
-	convertedData[targetIndex].status = status;
-	
-	setTimeout(() => {
-		console.log(`${targetName}가 ${status}로 상태가 변경되었습니다`);
-	}, 3000);
-	
-	setTimeout(() => {
-		show("all");
-	}, 1000);
-};
+    target.status = status;
 
-
-const loadData = (data) => {
-	const statusObject = {
-		"todo" :[],
-		"doing" : [],
-		"done" :[]
-	};
-	const makInitObj = (obj) => {
-		let data = {
-			name : obj.name,
-			id : obj.id
-		};
-		statusObject[obj.status].push(data);
-	};
-    data.forEach((obj) => {
-		
-        if (obj.status === "todo") { makInitObj(obj);
-        } else if (obj.status === "doing") { makInitObj(obj);
-        } else if (obj.status === "done") { makInitObj(obj);}
-    });
-	return statusObject;
-};
-
-const show = (todosType) => {
-	let statusObject = loadData(convertedData);
-    if (todosType === 'all') {
-        console.log(`todo : ${statusObject.todo.length}개, doing : ${statusObject.doing.length}개, done : ${statusObject.done.length}개`);
-    } else if (status.includes(todosType)) {
-        let listByStatus = statusObject[todosType];
-        let nameListOfStatus = listByStatus.reduce((init, value) => {
-            init.push(`'${value.name}, ${value.id}번'`);
-            return init;
-        },[]);
-		
-	console.log(`${todosType}리스트 :  총 ${listByStatus.length} : ${nameListOfStatus.join(', ')}`);
-    }
-};
+    setTimeout(() => {
+        console.log(`${target.name}가 ${status}로 상태가 변경되었습니다`);
+        showStatusLazy();
+    }, 3000);
+}
 
 module.exports = {
-    show,
+    "show" : showData,
 	"add" : addData,
 	"update" : updateData,
 	"delete" : deleteData,
