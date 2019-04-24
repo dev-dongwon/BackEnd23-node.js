4107991cd55e5415edf4cfa70e1eb3cfcf241262
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-24 15:55:44 +0900

    add method : excuteCmd
    execute command line to get the getCmdList argument

--- step5 - oop/mission/alone/decoder.js
+++ step5 - oop/mission/alone/decoder.js
@@ -2,22 +2,21 @@ function Decoder() {
     this.regexpList = {
         seperateUserinput : /[^\$]+/g,
     };
-    
 }
 
 Decoder.prototype = {
     getCmdList : (userInput, regexp) => {
         return userInput.match(regexp);
     },
-    // executeCmd : (cmdList) =>{
-    //     if (arguments.length != 1) return;
+    executeCmd : (cmdList) =>{
+        if (arguments.length != 1) return;
 
-    //     if (cmdList.length == 2) {
-    //         instruction[cmdList[0]](commandArr[1]);
-    //     } else if (cmdList.length == 3) {
-    //         instruction[cmdList[0]](cmdList[1], cmdList[2]);
-    //     }
-    // },
+        if (cmdList.length == 2) {
+            instruction[cmdList[0]](commandArr[1]);
+        } else if (cmdList.length == 3) {
+            instruction[cmdList[0]](cmdList[1], cmdList[2]);
+        }
+    },
 }
 
 module.exports = Decoder;
