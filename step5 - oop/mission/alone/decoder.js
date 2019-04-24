522e591000b20612dac8babcbb01f72f9bed7d87
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-24 15:54:04 +0900

    create decoder.js to make the decoder object

---
+++ step5 - oop/mission/alone/decoder.js
@@ -0,0 +1,23 @@
+function Decoder() {
+    this.regexpList = {
+        seperateUserinput : /[^\$]+/g,
+    };
+    
+}
+
+Decoder.prototype = {
+    getCmdList : (userInput, regexp) => {
+        return userInput.match(regexp);
+    },
+    // executeCmd : (cmdList) =>{
+    //     if (arguments.length != 1) return;
+
+    //     if (cmdList.length == 2) {
+    //         instruction[cmdList[0]](commandArr[1]);
+    //     } else if (cmdList.length == 3) {
+    //         instruction[cmdList[0]](cmdList[1], cmdList[2]);
+    //     }
+    // },
+}
+
+module.exports = Decoder;
