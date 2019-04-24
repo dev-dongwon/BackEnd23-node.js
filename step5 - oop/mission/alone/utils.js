1ed0df357da21f278b3892156a85c6c9e25f59aa
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-24 16:34:15 +0900

    add the commonUtils : common utils for todosApp
    add the delay function : delay the function in async status
    add the getArrByCondition Function : get Array by condition by reduce

--- step5 - oop/mission/alone/utils.js
+++ step5 - oop/mission/alone/utils.js
@@ -3,26 +3,26 @@ const Utils = () => {};
 const instructorUtils = {
 
     idGenerator : (max, min) => {
-        Math.floor(Math.random() * (max - min))
+        Math.floor(Math.random() * (max - min)) + 1;
     },
 
 }
 
-// const commonUtils = {
+const commonUtils = {
     
-//     delay : (time) => {
-//         return new Promise(function(resolve, reject){
-//             setTimeout(resolve, time);
-//         });
-//     },
+    delay : (time) => {
+        return new Promise(function(resolve, reject){
+            setTimeout(resolve, time);
+        });
+    },
 
-//     getArrByCondition : (arr, condition) => {
-//         return arr.reduce((acc, val) => {
-//             if (condition(val)) acc.push(val);
-//             return acc;
-//         }, []);
-//     },
-// }
+    getArrByCondition : (arr, condition) => {
+        return arr.reduce((acc, val) => {
+            if (condition(val)) acc.push(val);
+            return acc;
+        }, []);
+    },
+}
 
 Utils.prototype = {
     instructorUtils,
