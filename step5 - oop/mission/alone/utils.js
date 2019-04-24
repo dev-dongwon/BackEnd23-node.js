60273178c90777ae32a80001c60f686c5f526297
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-24 16:31:40 +0900

    create the Utils.js
    Utils Object to excute instructionUtils and CommonUtils

---
+++ step5 - oop/mission/alone/utils.js
@@ -0,0 +1,32 @@
+const Utils = () => {};
+
+const instructorUtils = {
+
+    idGenerator : (max, min) => {
+        Math.floor(Math.random() * (max - min))
+    },
+
+}
+
+// const commonUtils = {
+    
+//     delay : (time) => {
+//         return new Promise(function(resolve, reject){
+//             setTimeout(resolve, time);
+//         });
+//     },
+
+//     getArrByCondition : (arr, condition) => {
+//         return arr.reduce((acc, val) => {
+//             if (condition(val)) acc.push(val);
+//             return acc;
+//         }, []);
+//     },
+// }
+
+Utils.prototype = {
+    instructorUtils,
+    commonUtils
+}
+
+module.exports = Utils;
