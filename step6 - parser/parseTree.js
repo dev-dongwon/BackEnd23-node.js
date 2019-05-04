b775f68fe29d832d27e89bd1860c15a3a2a101b3
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-04 16:10:33 +0900

    Feat : insert 메서드 추가
    data를 받아 node로 변환
    이후 parseTree에 node를 추가

--- step6 - parser/parseTree.js
+++ step6 - parser/parseTree.js
@@ -12,4 +12,31 @@ const ParseTree = class {
     constructor() {
         this.root = null;
     }
+
+    insert(data, stack) {
+
+        const node = new Node(data);
+
+        if (this.root === null && node.type === parseUtils.dataType.startArray) {
+            this.root = {type : node.type, child : []};
+            stack.push(node);
+        
+        } else {
+            let parent = stack.peek();
+            
+            if (node.type === parseUtils.dataType.startArray) {
+                stack.push(node);
+                parent.child.push(node);
+            } else if (node.type === parseUtils.dataType.endArray) {
+                if(stack.isEmpty()) throw new Error('올바른 데이터 형식이 아닙니다 (끝나는 괄호가 더 많습니다)');
+                stack.pop();
+            } else {
+                parent.child.push(node);
+            }
+
+            this.root = parent;
+        }
+    }
 }
+
+module.exports = ParseTree;
