bf384878272d1b674954a2c5cc82a49abc432790
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-04 16:03:57 +0900

    Feat : getDataType function 추가
    데이터 타입 식별 및 반환

--- step6 - parser/parserUtils.js
+++ step6 - parser/parserUtils.js
@@ -6,6 +6,26 @@ const parserUtils = {
         seperator : 'seperator',
         whitespace : 'whitespace'
     },
+
+    getDataType(token) {
+
+        typeRegexp = {
+            number : /\d/g,
+            startArray : /[\[]/g,
+            endArray : /[\]]/g,
+            seperator : /[,]+/g,
+            whitespace : /\s+/g,
+        };
+
+        let dataType;
+
+        Object.keys(typeRegexp).forEach((key) => {
+            if (typeRegexp[key].test(token)) dataType = this.dataType[key];
+        })
+
+        return dataType;
+    },
+
 }
 
 module.exports = parserUtils;
