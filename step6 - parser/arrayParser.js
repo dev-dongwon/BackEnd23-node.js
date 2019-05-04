bc5110bd421a7ed13d2e7382154bd090fb359736
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-04 16:02:03 +0900

    Feat : tokenizer 메서드 추가
    문자열을 잘라 배열로 반환

--- step6 - parser/arrayParser.js
+++ step6 - parser/arrayParser.js
@@ -2,4 +2,31 @@ const ArrayParser = class {
     constructor(str) {
         this.str = str;
     }
+
+    tokenizer() {
+
+        const charArr = this.str.split("").filter((char) => !parserutils.isType(char, parserutils.dataType.whitespace));
+        const tokenArr = charArr.reduce((acc, val, index) => {
+            
+            let numberCount = 0;
+            
+            if (parserutils.isType(val, parserutils.dataType.number)) {
+                let [numberElement, idx] = [``, index];
+
+                while (parserutils.isType(charArr[idx], parserutils.dataType.number)) {
+                    [numberElement, idx, numberCount] = [numberElement+charArr[idx], idx+1, numberCount+1]
+                }
+                acc.push(numberElement);
+
+            } else {
+                acc.push(val);
+            }
+
+            charArr.splice(index, numberCount-1);
+            return acc;
+
+        }, []);
+
+        return tokenArr;
+    }
 }
