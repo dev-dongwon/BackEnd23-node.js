3dc2906c583b2ea9ecd07ea5d35917f9d5427489
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-04 16:09:27 +0900

    Feat : Node 클래스 추가
    type, value, child를 생성자로 가진 node 클래스 생성

--- step6 - parser/parseTree.js
+++ step6 - parser/parseTree.js
@@ -1,5 +1,13 @@
 const parseUtils = require('./parserUtils.js');
 
+const Node = class {
+    constructor(obj) {
+        this.type = obj.type;
+        this.value = obj.value;
+        this.child = [];
+    }
+}
+
 const ParseTree = class {
     constructor() {
         this.root = null;
