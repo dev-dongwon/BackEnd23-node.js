a61eaa5eb8ac092aff1737224db084c892c85a63
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-07 16:03:40 +0900

    Fix : tokenizer filter 함수 적용

--- step6 - parser/step6-2/arrayParser.js
+++ step6 - parser/step6-2/arrayParser.js
@@ -2,4 +2,10 @@ const ArrayParser = class {
     constructor(string) {
         this.string = string;
     }
+
+    tokenizer() {
+        return this.string.split("").filter((val) => val !== " ");
+    }
 }
+
+module.exports = ArrayParser;
