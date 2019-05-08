bb5420c8718e295211de1cb757a60b4ad4002993
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-08 20:43:56 +0900

    Test : tokenizer 테스트 케이스 작성
    숫자, 문자열, boolean, 혼합 데이터로 구성된 테스트케이스 추가
    테스트케이스 실행 및 작동 여부 SUCCESS 확인

--- step6 - parser/step6-2/unitTest.js
+++ step6 - parser/step6-2/unitTest.js
@@ -4,27 +4,32 @@ const ArrayParser  = require('./arrayParser');
 const ArrayParserTest = () => {
 
     const tokenizerTest = () => {
-        
-        const numberTestCases = [
+        const testStr = [
             "[1,2,3,4,5]",
-            "  [1, 2, 3, 4, 5]",
-            "[1, 2, 3, 4 ,   5   ]",
+            "['hi'']",
+            "[true, false]",
+            "['1a3',[null,false,['11',[112233],112],55,'99'],33, true]"
         ]
         
-        const expectedNumber = ['[', '1', ',', '2', ',', '3', ',', '4', ',', '5', ']'];
-        
-        const stringTestCases = [
-            "['lion', 'tiger', 'crong', 'honux']"
+        const testCases = testStr.reduce((acc, str) => {
+            const arrayParser = new ArrayParser(str);
+            acc.push(arrayParser.tokenizer());
+            return acc;
+        }, [])
+
+        const expectedCases = [
+            ['[', '1', '2', '3', '4', '5', ']'],
+            ['[', '\'hi\'', '\'', ']'],
+            ['[', 'true', 'false', ']'],
+            [ '[', '\'1a3\'', '[', 'null', 'false', '[', '\'11\'', '[', '112233', ']', '112', ']', '55', '\'99\'', ']', '33', 'true', ']']
         ]
 
-        numberTestCases.forEach((str) => {
-            const arrayParser = new ArrayParser(str);
-            test.assertArrayEquals(expectedNumber, arrayParser.tokenizer(), tokenizerTest);
+        testCases.forEach((testCase, index) => {
+            test.assertArrayEquals(testCases[index], expectedCases[index], tokenizerTest)
         })
     }
 
     tokenizerTest();
 }
-
 
 ArrayParserTest();
