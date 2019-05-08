ab69f96cd0fbbd1425023d4719f9a8de769f1bc8
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-08 20:42:32 +0900

    Feat : tokenizer 기능 구현
    문자열을 split해 데이터 단위인 token으로 만든 후 배열로 반환

--- step6 - parser/step6-2/arrayParser.js
+++ step6 - parser/step6-2/arrayParser.js
@@ -1,14 +1,65 @@
+const utils = require('./utils');
+const Stack = require('./stack');
+
 const ArrayParser = class {
     constructor(string) {
         this.string = string;
+        this.tokenizedArr = this.tokenizer();
     }
 
     tokenizer() {
-        const tokenArr = [];
 
-        this.string.split("").filter((val) => val !== " ")
+        const tokenArr = [];
+        const quoteStack = new Stack();
+        let token = ``;
+
+        const pushToken = () => {
+            if (token !== ``) {
+                tokenArr.push(token);
+            }
+            token = ``;
+        }
+
+        this.string.split("").forEach((char) => {
+            
+            switch (char) {
+                case '[':
+                    tokenArr.push(char);
+                    break;
+                case ']':
+                    pushToken();
+                    tokenArr.push(char);
+                    break;
+                case '\'':
+                    token += char;
+                    if (quoteStack.isEmpty()) {
+                        quoteStack.push(char);
+                    } else {
+                        quoteStack.pop();
+                        pushToken();
+                    }
+                    break;
+                case ',':
+                    if (quoteStack.isEmpty()) {
+                        pushToken();
+                    } else {
+                        token += char;
+                    }
+                    break;
+                case ' ':
+                    if (!quoteStack.isEmpty()) {
+                        token += char;
+                    }
+                    break;
+                default:
+                    token += char;
+                    break;
+            }
+        })
         
+        return tokenArr;
     }
+
 }
 
 module.exports = ArrayParser;
