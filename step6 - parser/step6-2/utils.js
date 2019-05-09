294834393c5e147e835321269397a7f02d627bf9
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-09 17:18:48 +0900

    Feat : 데이터 타입을 반환하는 getDataType 메서드 추가

--- step6 - parser/step6-2/utils.js
+++ step6 - parser/step6-2/utils.js
@@ -32,6 +32,38 @@ const utils = {
         return quoteStack.isEmpty();
     },
 
+    getDataType(token) {
+        let type = undefined;
+
+        switch (true) {
+            case this.typesRegexp.number.test(token):
+                type = 'number';
+                break;
+            case this.isString(token):
+                type = 'string';
+                break;
+            case this.typesRegexp.startArray.test(token):
+                type = 'array';
+                break;
+            case this.typesRegexp.endArray.test(token):
+                type = 'endArray';
+                break;
+            case this.typesRegexp.boolean.test(token):
+                type = 'boolean';
+                break;
+            case this.typesRegexp.null.test(token):
+                type = 'null';
+                break;
+            default:
+                break;
+        }
+
+        if (type === undefined) {
+            throw new Error(`[[  ${token}  ]]은 올바른 데이터 타입이 아닙니다`);
+        }
+
+        return type;
+    }
 }
 
 module.exports = utils;
