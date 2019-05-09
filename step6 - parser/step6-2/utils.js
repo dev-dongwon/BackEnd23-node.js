1200e475eda99e29baaa9ead022fcfa61516c3c6
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-09 17:18:12 +0900

    Feat : string을 판별하는 isString 메서드 추가

--- step6 - parser/step6-2/utils.js
+++ step6 - parser/step6-2/utils.js
@@ -1,12 +1,37 @@
+const Stack = require('./stack');
+
 const utils = {
     
-    dataType : {
-        number : /\d+/,
+    typesRegexp : {
+        startArray : /[\[]/,
+        endArray : /[\]]/,
+        number : /^[-|+]?\d*[.]?\d+/,
         string : /\S+/,
-        boolean : /[true|false]/,
-        null : /[null]/,
+        boolean : /true|false/,
+        null : /null/,
+    },
+    
+    isString(token) {
+        const quoteStack = new Stack();
+        const charArr = token.split("");
+
+        if (charArr[0] !== '\'') {
+            return false;
+        }
+        
+        token.split("").forEach((char) => {
+            if (quoteStack.isEmpty()) {
+                quoteStack.push(char);
+            } else {
+                if (char === '\'') {
+                    quoteStack.pop();
+                }
+            }
+        });
+
+        return quoteStack.isEmpty();
     },
 
-    
-
 }
+
+module.exports = utils;
